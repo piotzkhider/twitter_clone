1b07e2af8a0a670444cf8c482d23e3e4a472ff8e
yoshihiro sato <sato@asia-quest.jp>
2017-07-25 19:04:19 +0900

    リファクタリング

--- resources/views/pieces/timeline.blade.php
+++ resources/views/pieces/timeline.blade.php
@@ -12,7 +12,7 @@
     </li>
 
     <li class="media list-group-item p-4">
-        <article class="d-flex">
+        <article class="d-flex w-100">
             <img class="media-object d-flex align-self-start mr-3"
                  src="{{ asset('images/no-thumb.png') }}">
             <div class="media-body">
@@ -22,14 +22,15 @@
                 </div>
 
                 <p>
-                    Aenean lacinia bibendum nulla sed consectetur. Vestibulum id ligula porta felis
-                    euismod
-                    semper. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras justo
-                    odio,
-                    dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod
-                    semper.
-                    Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus
-                    mus.
+                    mus
+                    {{--Aenean lacinia bibendum nulla sed consectetur. Vestibulum id ligula porta felis--}}
+                    {{--euismod--}}
+                    {{--semper. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras justo--}}
+                    {{--odio,--}}
+                    {{--dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod--}}
+                    {{--semper.--}}
+                    {{--Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus--}}
+                    {{--mus.--}}
                 </p>
             </div>
         </article>

79990d3c7929e9202bc57ed1c3ddf2b1d60ca936
sato <sato@asia-quest.jp>
2017-08-08 05:53:58 +0900

    アバウト部分のリファクタリング

--- resources/views/fragments/about.blade.php
+++ resources/views/fragments/about.blade.php
@@ -2,14 +2,26 @@
     <div class="card-block">
         <h6 class="card-title">
             About
-            <small>· <a href="#">Edit</a></small>
+            @if($me->equals($user))
+                <small>· <a href="{{ route('settings.profile') }}">Edit</a></small>
+            @endif
         </h6>
         <ul class="list-unstyled ayn">
-            <li><span class="text-muted icon icon-calendar mr-3"></span>Went to <a href="#">Oh, Canada</a>
-            <li><span class="text-muted icon icon-users mr-3"></span>Became friends with <a href="#">Obama</a>
-            <li><span class="text-muted icon icon-github mr-3"></span>Worked at <a href="#">Github</a>
-            <li><span class="text-muted icon icon-home mr-3"></span>Lives in <a href="#">San Francisco, CA</a>
-            <li><span class="text-muted icon icon-location-pin mr-3"></span>From <a href="#">Seattle, WA</a>
+            @if($user->link)
+                <li class="mb-1"><span class="text-muted icon icon-link mr-3"></span>
+                    <a href="{{ $user->link }}">https://www.asia-quest.jp/</a>
+                </li>
+            @endif
+            @if($user->organization)
+                <li class="mb-1"><span class="text-muted icon icon-users mr-3"></span>
+                    {{ $user->organization }}東京大学
+                </li>
+            @endif
+            @if($user->address)
+                <li class="mb-1"><span class="text-muted icon icon-location-pin mr-3"></span>
+                    {{ $user->address }}名古屋
+                </li>
+            @endif
         </ul>
     </div>
 </div>
